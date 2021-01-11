f72d688c037aa02704fc2d7ebf83d65918c5f6f9
sitaram.iyer <sitaramkm@gmail.com>
2021-01-11 10:17:12 -0600

    Updates to optimize code

--- ec2-operator.py
+++ ec2-operator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
+import datetime
+
 import boto3
 import croniter
-import datetime
-from botocore.exceptions import ClientError
 
 # regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2",
 #           "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1",
@@ -11,11 +11,11 @@ from botocore.exceptions import ClientError
 
 regions = ["us-east-1"]
 
-bo
+
 # return true if the cron schedule falls between now and now+seconds
-def time_to_action(sched, time_now, seconds):
+def time_to_action(ec2_schedule, time_now, seconds):
     try:
-        cron = croniter.croniter(sched, time_now)
+        cron = croniter.croniter(ec2_schedule, time_now)
         d1 = time_now + datetime.timedelta(0, seconds)
         if seconds > 0:
             d2 = cron.get_next(datetime.datetime)
@@ -30,7 +30,6 @@ def time_to_action(sched, time_now, seconds):
         ret = False
     print("time_to_action %s" % ret)
     return ret
-
 
 
 # time_to_action("0 0 * * SAT", now, 31 * 60)
@@ -53,34 +52,34 @@ for region in regions:
 
                 # check auto:start and auto:stop tags
                 if 'auto:start' in tag['Key']:
-                    start_sched = tag['Value']
+                    start_schedule = tag['Value']
                 else:
-                    start_sched = None
+                    start_schedule = None
 
                 if 'auto:stop' in tag['Key']:
-                    stop_sched = tag['Value']
+                    stop_schedule = tag['Value']
                 else:
-                    stop_sched = None
+                    stop_schedule = None
 
             state = instance.state['Name']
             #  print('region={}'.format(region), 'instance-name={}'.format(name),
             #        'instance-type={}'.format(instance.instance_type), 'instance-id={}'.format(instance.id),
             #        'instance-state={}'.format(state))
-            print('instance-laumch-time={}'.format(instance.launch_time))
+            print('instance-launch-time={}'.format(instance.launch_time))
             print('instance-tags={}'.format(instance.tags))
             # print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %  name, instance.id, instance.instance_type,
-            #     instance.launch_time, state, start_sched, stop_sched, instance.tags)
+            #     instance.launch_time, state, start_schedule, stop_schedule, instance.tags)
 
             # queue up instances that have the start time falls between now and the next 30 minutes
             # Starting instances won't work as the filter specifically is looking for running instances and
             # will not find anything to start. This is as-designed at this time.
 
-            if start_sched != None and state == "stopped" and time_to_action(start_sched, now, 31 * 60):
+            if start_schedule is not None and state == "stopped" and time_to_action(start_schedule, now, 31 * 60):
                 start_list.append(instance.id)
 
             # queue up instances that have the stop time falls between 30 minutes ago and now
-            # if stop_sched != None and state == "running" and time_to_action(stop_sched, now, 31 * -60):
-            if stop_sched != None and state == "running":
+            # if stop_schedule != None and state == "running" and time_to_action(stop_schedule, now, 31 * -60):
+            if stop_schedule is not None and state == "running":
                 stop_list.append(instance.id)
 
         # start instances
@@ -98,4 +97,4 @@ for region in regions:
 
     # most likely will get exception on new beta region and gov cloud
     except Exception as e:
-        print('Error {}}',  e)
+        print('Error {}}', e)
